92687e7dbf6698bf919c0b66b2defe3091344fa1
Ben Holgate <holgateb@hotmail.com>
2022-06-22 18:31:21 -0700

    uploaded psuedo code and starter code

---
+++ script.js
@@ -0,0 +1,18 @@
+//DECLARE current time
+
+//DECLARE currrent day element
+
+//DECLARE time entries container element
+
+//Render a black for each hour of the day
+
+var myArray = ["asdf", "asdf"]
+for(var i = 0)
+
+//attempt to get the saved data for the hour of loop
+var data = ["asdf"]
+
+
+//APPEND html to page
+
+// save an hour to local storage
