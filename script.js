b45e0ecf464c01e3079004e3f6829aaf8abb291e
Ben Holgate <holgateb@hotmail.com>
2022-07-06 17:56:54 -0700

    added date.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+var containerEL = document.querySelector("timeblocks")
+
 //DECLARE current time
+
+//DECLARE current hour
+
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
 
 //DECLARE currrent day element
 
@@ -6,13 +13,52 @@
 
 //Render a black for each hour of the day
 
-var myArray = ["asdf", "asdf"]
-for(var i = 0)
+// var myArray = ["asdf", "asdf"] {
+for(var i = 0; i > today.length; i++){
+    
+};
+
+//compare i to currrent hour to determine if this hour is past present future
 
 //attempt to get the saved data for the hour of loop
-var data = ["asdf"]
+// var data = "hour-"+i
+
+// var data = ["asdf"]
 
 
-//APPEND html to page
+//APPEND html to container element
 
-// save an hour to local storage
+// }
+
+//set up a click event listener on the container
+
+containerEL.on("click", "[data-hour]", function(event) {
+
+    //fetch the hour from the clicked buttons (event.taget) "data-hour" attribute.
+
+    //use the hour to create the key for local storage.
+
+    //from the clicked button, traverse the DOM to the nearby textarea to fetch it's value
+
+    //Use the key and the value into localstorage
+
+});
+
+    // save an hour to local storage
+
+
+
+
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
